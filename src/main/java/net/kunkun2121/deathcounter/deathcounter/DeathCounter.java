bcde96be9b46b0c4553ef8f4ede7253c57a185db
KUNKUN2121 <kunkun7532@gmail.com>
2022-01-31 08:41:38 +0900

    1

--- src/main/java/net/kunkun2121/deathcounter/deathcounter/DeathCounter.java
+++ src/main/java/net/kunkun2121/deathcounter/deathcounter/DeathCounter.java
@@ -1,6 +1,8 @@
 package net.kunkun2121.deathcounter.deathcounter;
 
-import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,28 +12,44 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DeathCounter extends JavaPlugin implements Listener {
 
-    @Override
+    @Override   //開始処理
     public void onEnable() {
         // Plugin startup logic
         saveDefaultConfig();
-        FileConfiguration config = getConfig();
-
-
         getServer().getPluginManager().registerEvents(this,this);
-
     }
 
-    @EventHandler
+    @Override   //終了処理
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler //ログイン処理
+    public void onJoin(PlayerJoinEvent e){
+        Player player =e.getPlayer();
+        String mcid = player.getName().toLowerCase();
+        if(!this.getConfig().isSet("Players." + mcid)){
+            this.getConfig().set("Players." + mcid, 0);
+            getServer().broadcastMessage("追加しました。");
+            this.saveConfig();
+        }else {
+            getServer().broadcastMessage("追加しませんでした。");
+            this.saveConfig();
+        }
+    }
+
+    @EventHandler   //死亡イベント
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
-
-        if(this.getConfig().isSet("Players." + player.getName())) {
+        String mcid = player.getName().toLowerCase();
+        if(this.getConfig().isSet("Players." + mcid)) {
             reloadConfig();
-            int dc = this.getConfig().getInt("Players." + player.getName(),9999);
-            if(dc != 9999) {
-                this.getConfig().set("Players." + player.getName(), dc + 1);
-                System.out.println(dc);
+            int dc = this.getConfig().getInt("Players." + mcid,-1);
+            if(dc != -1) {
                 dc=dc+1;
+                this.getConfig().set("Players." + mcid, dc);
+                System.out.println(dc);
+
                 getServer().broadcastMessage(player.getName()+"さんの死亡回数は" + dc + "回目です。");
                 this.saveConfig();
             }else{
@@ -39,20 +57,44 @@ public final class DeathCounter extends JavaPlugin implements Listener {
             }
         }
     }
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e){
-        Player player =e.getPlayer();
-        if(!this.getConfig().isSet("Players." + player.getName())){
-            this.getConfig().set("Players." + player.getName(), 0);
-//            getServer().broadcastMessage("追加しました。");
-            this.saveConfig();
-        }else {
-//            getServer().broadcastMessage("追加しませんでした。");
-            this.saveConfig();
-        }
+
+    @Override   //コマンド処理
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("DeathCounter")) {
+            if (args.length == 0) {
+                // 引数nasi
+                sender.sendMessage("使い方");
+                sender.sendMessage("&4---------------------------------");
+                sender.sendMessage("/DeathCounter set name count");
+                sender.sendMessage("/DeathCounter view name");
+                sender.sendMessage("/DeathCounter ranking");
+                sender.sendMessage("&4---------------------------------");
+                return true;
+            }
+            if(args[0].equalsIgnoreCase("set")) { //set入力
+                String mcid = args[1].toLowerCase( ); //set mcid
+//                if(this.getConfig().isSet("Players." + name) { //mcidあるか
+                    int dc = Integer.parseInt(args[2]);             //deathcount
+                    sender.sendMessage(" "+mcid+" "+ dc);                    //debag
+                    this.getConfig().set("Players." + mcid, dc);//設定
+                    int test = this.getConfig().getInt("Players."+mcid);
+                    sender.sendMessage(""+test);
+                    this.saveConfig();
+                return true;
+                }
+            }
+
+
+            if(args[0].equalsIgnoreCase("view")) {
+                sender.sendMessage("view");
+                return true;
+            }
+            if(args[0].equalsIgnoreCase("ranking")) {
+                sender.sendMessage("ranking");
+                return true;
+            }
+        return false;
     }
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+
 }
+
